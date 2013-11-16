424111cd92fe086189e6aff366aca1730e1b6803
RemyD <remyd@civx.us>
2013-11-16 15:43:58 -0500

    Added flask server bits

---
+++ run.py
@@ -0,0 +1,45 @@
+"""
+Author: Remy D <decause+printlure@gmail.com>
+        Ralph Bean <rbean@redhat.com>
+        Anthony D <decausem@gmail.com>
+        Sean Crowder <s7crowder@gmail.com>
+License: AGPL
+"""
+import os
+
+from flask import Flask
+from flask.ext.mako import MakoTemplates, render_template
+from flask import redirect, url_for
+
+app = Flask(__name__)
+app.template_folder = "templates"
+mako = MakoTemplates(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html', name='mako')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.mak', name='mako')
+
+@app.route('/lures')
+def lures():
+    return render_template('lures.mak', name='mako')
+
+
+@app.route('/story')
+def story():
+    return render_template('story.mak', name='mako')
+    #return redirect(url_for('static', filename='img/preso.svg'))
+
+
+if __name__ == "__main__":
+    if 'OPENSHIFT_PYTHON_IP' in os.environ:
+        host = os.environ['OPENSHIFT_PYTHON_IP']
+        port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+        app.run(host=host, port=port)
+    else:
+        app.debug = True
+        app.run()
